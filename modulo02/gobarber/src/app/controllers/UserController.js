fb6784f55cc47eeb7ab67af4401d47475cbedd2f
Skaylander <lenzxavier@gmail.com>
2020-02-03 22:05:26 -0300

    User update

--- modulo02/gobarber/src/app/controllers/UserController.js
+++ modulo02/gobarber/src/app/controllers/UserController.js
@@ -23,7 +23,28 @@ class UserController {
 
     const user = await User.findByPk(req.userId);
 
-    return res.json({ ok: true });
+    if (email && email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
