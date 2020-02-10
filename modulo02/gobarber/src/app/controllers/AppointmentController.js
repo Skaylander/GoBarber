9cc59b1029bd65eb64ea82f3e97136950a9ec95f
Skaylander <lenzxavier@gmail.com>
2020-02-10 02:46:03 -0300

    Agenda parte 1

--- modulo02/gobarber/src/app/controllers/AppointmentController.js
+++ modulo02/gobarber/src/app/controllers/AppointmentController.js
@@ -1,5 +1,6 @@
-import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
+import * as Yup from 'yup';
+
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
